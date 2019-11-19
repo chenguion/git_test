8184f3774f88ac8a8fde3a6ce73905927e018c3d
Chenguion <2522273441@qq.com>
2019-11-19 20:10:23 +0800

    创建视图login

--- views.py
+++ views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import rediect
+
 
 def index(request):
     return HttpResponse('index') 
+
+def login(request):
+    return rediect('/index')
+
